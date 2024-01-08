a009c4d1a6a6b13f4d505fafbd5eaedc6fbe964b
Vyacheslav <bryzgachev.vyach@yandex.ru>
2024-01-08 21:50:21 +0300

    diplom part2 main-service v1.2

--- ewm-stat-service/stat_server/src/main/java/ru/practicum/controller/HitController.java
+++ ewm-stat-service/stat_server/src/main/java/ru/practicum/controller/HitController.java
@@ -1,10 +1,12 @@
 package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.dto.StatDtoHitResponse;
 import ru.practicum.dto.StatDtoRequest;
@@ -20,6 +22,7 @@ public class HitController {
     private final StatService statService;
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public StatDtoHitResponse createHit(@Valid @RequestBody StatDtoRequest statsDto) {
         return statService.create(statsDto);
     }
